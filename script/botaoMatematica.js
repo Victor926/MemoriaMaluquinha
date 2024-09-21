72a45df53237fc6ed58fec0c7380dd5c02056d51
Patrick Furquim <patrickfurquim357@gmail.com>
2024-09-21 12:16:17 -0300

    Finalizei a lógica no jquery

--- script/botaoMatematica.js
+++ script/botaoMatematica.js
@@ -1,6 +1,44 @@
 $(document).ready(function () {
-    $(".opc").click(function(){
+    var primeiro = null;
+    var segundo = null;
+    var contagem = 0;
+
+    
+    $(".ativado").click(function(){
+        if(primeiro === $(this)){
+            return;
+        }
+        if(primeiro === null)
+        {
+            $(this).toggleClass("rotate3d");
+            primeiro = $(this);
+            return;
+        }
         $(this).toggleClass("rotate3d");
+        segundo = $(this);
+        if(primeiro.data('id') === segundo.data('id'))
+        {
+            $(primeiro).off('click');
+            $(segundo).off('click');
+            contagem++;
+            contagem++;
+            if(contagem === 8){
+                alert("Parabens voce ganhou!!");
+            }
+            primeiro = null;
+            segundo = null;
+        }
+        else{
+            setTimeout(function() {
+                $(primeiro).toggleClass("rotate3d");
+                $(segundo).toggleClass("rotate3d");
+                primeiro = null;
+                segundo = null;
+            }, 1000); 
+        }
         
     });
+
+    
+
 });
